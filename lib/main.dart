56f50e9ab3ae0ea72db4a43a6e53407cd24994dc
s4rath <sarathj810@gmail.com>
2023-03-29 09:56:57 +0530

    pages created

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:trash_can/detect.dart';
+import 'package:trash_can/faq.dart';
+import 'package:trash_can/home.dart';
+import 'package:trash_can/waste.dart';
+
 
 void main() {
   runApp(const MyApp());
@@ -12,104 +19,184 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      theme: ThemeData(),
+      home: Nav()
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class Nav extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _NavState createState() => _NavState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _NavState extends State<Nav> {
+  int currentIndex = 0;
+  String currentTitle='';
 
-  void _incrementCounter() {
+  setBottomBarIndex(index) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      currentIndex = index;
+      if(index==0)
+        currentTitle="Home";
+      else if(index==1)
+        currentTitle="Classify";
+
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    final Size size = MediaQuery.of(context).size;
+    final  List<Widget> _pages = <Widget>[
+      HomePage(),
+      Waste(),
+      Predict(),
+      FAQs(),
+    ];
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      backgroundColor: Color(0xFFFFFDD0),
+      body: Stack(
+        children: [
+          Center(
+            child: _pages[currentIndex],
+          ),
+          Positioned(
+            bottom: 0,
+            left: 0,
+            child: Container(
+              width: size.width,
+              height: 80,
+              child: Stack(
+                clipBehavior: Clip.none,
+                children: [
+                  CustomPaint(
+                    size: Size(size.width, 80),
+                    painter: BNBCustomPainter(),
+                  ),
+                  Column(
+                    children: [
+                      Center(
+                        heightFactor: 0.6,
+                        child: FloatingActionButton( backgroundColor:Colors.black.withOpacity(0.7),child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Image.asset('assets/logo.png',fit: BoxFit.fill,),
+                        ) ,onPressed: () {
+                          setBottomBarIndex(2);
+                        },),
+                      ),
+                      SizedBox(height: 26,),
+                      Text('Classify',style: GoogleFonts.getFont('Didact Gothic',color:Colors.black,fontWeight: FontWeight.bold,fontSize: 14),),
+                    ],
+                  ),
+                  Container(
+                    width: size.width,
+                    height: 80,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Column(
+                          mainAxisAlignment:MainAxisAlignment.center,
+                          children: [
+                            IconButton(
+                              icon: Icon(
+                                FontAwesomeIcons.house,
+                                color: currentIndex == 0 ? Colors.grey : Colors.black,
+                                size: 25,
+                              ),
+                              onPressed: () {
+                                setBottomBarIndex(0);
+                              },
+                              splashColor: Colors.white,
+                            ),
+                            Text('Home',style: GoogleFonts.getFont('Didact Gothic',color: currentIndex == 0 ? Colors.grey : Colors.black,fontWeight: FontWeight.bold),),
+                          ],
+                        ),
+                        Column(
+                          mainAxisAlignment:MainAxisAlignment.center,
+                          children: [
+                            IconButton(
+                              icon: Icon(
+                                FontAwesomeIcons.glassWater,
+                                color: currentIndex == 1 ? Colors.grey : Colors.black,
+                                size: 25,
+                              ),
+                              onPressed: () {
+                                setBottomBarIndex(1);
+                              },
+                              splashColor: Colors.white,
+                            ),
+                            Text('Tips',style: GoogleFonts.getFont('Didact Gothic',color: currentIndex == 1 ? Colors.grey : Colors.black,fontWeight: FontWeight.bold),),
+                          ],
+                        ),
+                        Container(
+                          width: size.width * 0.2,
+                        ),
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            IconButton(
+                                icon: Icon(
+                                  FontAwesomeIcons.circleInfo,
+                                  color: currentIndex == 3 ? Colors.grey : Colors.black,
+                                  size: 25,
+                                ),
+                                onPressed: () {
+                                  setBottomBarIndex(3);
+                                }),
+                            Text('  FAQs',style: GoogleFonts.getFont('Didact Gothic',color: currentIndex == 3 ? Colors.grey : Colors.black,fontWeight: FontWeight.bold),),
+                          ],
+                        ),
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            IconButton(
+                                icon: Icon(
+                                  FontAwesomeIcons.peopleGroup,
+                                  color: currentIndex == 4 ? Colors.grey : Colors.black,
+                                  size: 25,
+                                ),
+                                onPressed: () {
+                                  setBottomBarIndex(4);
+                                }),
+                            Text('  About',style: GoogleFonts.getFont('Didact Gothic',color: currentIndex == 4 ? Colors.grey : Colors.black ,fontWeight: FontWeight.bold),),
+                          ],
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+          )
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+}
+class BNBCustomPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    Paint paint = new Paint()
+      ..color = Color(0xFFF7CC00)
+      ..style = PaintingStyle.fill;
+
+    Path path = Path();
+    path.moveTo(0, 20); // Start
+    path.quadraticBezierTo(size.width * 0.20, 0, size.width * 0.35, 0);
+    path.quadraticBezierTo(size.width * 0.40, 0, size.width * 0.40, 20);
+    path.arcToPoint(Offset(size.width * 0.60, 20), radius: Radius.circular(20.0), clockwise: false);
+    path.quadraticBezierTo(size.width * 0.60, 0, size.width * 0.65, 0);
+    path.quadraticBezierTo(size.width * 0.80, 0, size.width, 20);
+    path.lineTo(size.width, size.height);
+    path.lineTo(0, size.height);
+    path.lineTo(0, 20);
+    canvas.drawShadow(path, Colors.black, 5, true);
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return false;
   }
 }
